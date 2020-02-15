2170a40fc011582861063129f553a472ee60e4f0
Fliper Gustavo <gustavo.guedes@fliper.app>
2020-02-15 11:28:52 -0300

    routes

--- lib/routes/route_consts.dart
+++ lib/routes/route_consts.dart
@@ -1,4 +1,5 @@
-const String profile = '/profile';
-const String home = '/home';
-const String login = '/login';
+const String profile = 'profile';
+const String home = 'home';
+const String login = 'login';
 const String editProfile = 'edit_profile';
+const String devsList = 'devs_list';
--- lib/routes/route_generator.dart
+++ lib/routes/route_generator.dart
@@ -1,24 +1,44 @@
 import 'package:flutter/material.dart';
 import 'package:omni_app/routes/route_consts.dart';
-import 'package:omni_app/ui/screens/edit_profile.dart';
+import 'package:omni_app/ui/screens/drawer_menu_screens/devs_list/devs_list.dart';
+import 'package:omni_app/ui/screens/drawer_menu_screens/profile_screens/edit_profile.dart';
+import 'package:omni_app/ui/screens/drawer_menu_screens/profile_screens/profile.dart';
 import 'package:omni_app/ui/screens/home.dart';
 import 'package:omni_app/ui/screens/login.dart';
-import 'package:omni_app/ui/screens/profile.dart';
 
 class RouterGenerator {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case home:
-        return MaterialPageRoute<HomeScreen>(builder: (_) => HomeScreen());
+        return MaterialPageRoute<HomeScreen>(
+          builder: (_) => HomeScreen(),
+          settings: RouteSettings(name: home),
+        );
       case profile:
         return MaterialPageRoute<ProfileScreen>(
-            builder: (_) => ProfileScreen());
+          builder: (_) => ProfileScreen(),
+          settings: RouteSettings(name: profile),
+        );
       case login:
-        return MaterialPageRoute<LoginScreen>(builder: (_) => LoginScreen());
+        return MaterialPageRoute<LoginScreen>(
+          builder: (_) => LoginScreen(),
+          settings: RouteSettings(name: login),
+        );
       case editProfile:
-        return MaterialPageRoute<EditProfile>(builder: (_) => EditProfile());
+        return MaterialPageRoute<EditProfile>(
+          builder: (_) => EditProfile(),
+          settings: RouteSettings(name: editProfile),
+        );
+      case devsList:
+        return MaterialPageRoute<DevList>(
+          builder: (_) => DevList(),
+          settings: RouteSettings(name: devsList),
+        );
       default:
-        return MaterialPageRoute<Container>(builder: (_) => Container());
+        return MaterialPageRoute<Container>(
+          builder: (_) => Container(),
+          settings: RouteSettings(name: '404'),
+        );
     }
   }
 }
