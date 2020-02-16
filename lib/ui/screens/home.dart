f9f47490d057e277312429fb0c94b1591464debe
Fliper Gustavo <gustavo.guedes@fliper.app>
2020-02-16 19:08:34 -0300

    location package

--- lib/ui/screens/home.dart
+++ lib/ui/screens/home.dart
@@ -1,9 +1,24 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:omni_app/ui/components/drawer_menu.dart';
 import 'package:omni_app/ui/styles/colors.dart';
-import 'package:omni_app/ui/styles/styles.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(-23.6363776, -46.6419712),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(-23.6363776, -46.6419712),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,14 +26,20 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.black,
         appBar: AppBar(
           backgroundColor: primaryDarkColor,
-          elevation: 4,
+          elevation: 10,
           iconTheme: IconThemeData(color: Colors.white),
         ),
         body: Stack(children: <Widget>[
           Container(
             height: double.infinity,
             width: double.infinity,
-            color: Colors.blue,
+            child: GoogleMap(
+              mapType: MapType.normal,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+            ),
           ),
           Padding(
             padding: EdgeInsets.only(top: 20),
@@ -27,14 +48,36 @@ class HomeScreen extends StatelessWidget {
               children: <Widget>[
                 Container(
                   width: 250,
-                  color: Colors.transparent,
+                  decoration: BoxDecoration(
+                      color: Colors.white.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(45)),
                   child: TextFormField(
                     style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontFamily: 'Roboto',
+                        color: Colors.black,
+                        fontSize: 21,
+                        fontWeight: FontWeight.bold),
+                    decoration: InputDecoration(
+                      labelText: 'Tech',
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(45),
+                          borderSide: BorderSide(color: secondColor, width: 2)),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(45),
+                          borderSide: BorderSide(color: secondColor, width: 2)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(45),
+                        borderSide: BorderSide(
+                          width: 1,
+                          color: Colors.white,
+                        ),
+                      ),
+                      labelStyle: TextStyle(
+                          fontSize: 20,
+                          color: secondColor,
+                          fontWeight: FontWeight.bold),
+                      contentPadding: EdgeInsets.only(
+                          top: 20, right: 10, bottom: 20, left: 40),
                     ),
-                    decoration: inputStyle(labelText: 'Tech'),
                   ),
                 ),
                 SizedBox(
